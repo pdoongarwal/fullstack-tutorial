e3c441412951a2065cde46880ecbf29bf6706d81
Prabhat Doongarwal <pdoongarwal@gmail.com>
2020-10-17 14:50:59 +0530

    Add query resolvers

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -6,6 +6,33 @@ class LaunchAPI extends RESTDataSource {
     this.baseURL = "https://api.spacexdata.com/v2/";
   }
 
+  launchReducer(launch) {
+    const {
+      flight_number,
+      launch_date_unix,
+      launch_site,
+      mission_name,
+      links,
+      rocket,
+    } = launch;
+
+    return {
+      id: flight_number || 0,
+      cursor: launch_date_unix,
+      site: launch_site && launch_site.site_name,
+      mission: {
+        name: mission_name,
+        missionPatchSmall: links.mission_patch_small,
+        missionPatchLarge: links.mission_patch,
+      },
+      rocket: {
+        id: rocket.rocket_id,
+        name: rocket.rocket_name,
+        type: rocket.rocket_type,
+      },
+    };
+  }
+
   async getAllLaunches() {
     const response = await this.get("launches");
     return Array.isArray(response)
@@ -13,32 +40,12 @@ class LaunchAPI extends RESTDataSource {
       : [];
   }
 
-  launchReducer(launch) {
-    const { flight_number, launch_date_unix } = launch;
-
-    return {
-      id: flight_number || 0,
-      cursor: launch_date_unix,
-      site: launch.launch_site && launch.launch_site.site_name,
-      mission: {
-        name: launch.mission_name,
-        missionPatchSmall: launch.links.mission_patch_small,
-        missionPatchLarge: launch.links.mission_patch,
-      },
-      rocket: {
-        id: launch.rocket.rocket_id,
-        name: launch.rocket.rocket_name,
-        type: launch.rocket.rocket_type,
-      },
-    };
-  }
-
   async getLaunchById({ launchId }) {
     const response = await this.get("launches", { flight_number: launchId });
     return this.launchReducer(response[0]);
   }
 
-  getLaunchesByIds({ launchIds }) {
+  async getLaunchesByIds({ launchIds }) {
     return Promise.all(
       launchIds.map((launchId) => this.getLaunchById({ launchId }))
     );
